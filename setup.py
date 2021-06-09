26d71358f17795fb88ee279030243439dc642bed
connerlambden <9061871+connerlambden@users.noreply.github.com>
2021-06-09 21:08:34 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setup(
                       'jit',
                       'networkx',
                       'anndata',
-                      'scipy'                   
+                      'scipy',
+                      'matplotlib',
+                      'pandas'               
                       ],
 
     classifiers=[
